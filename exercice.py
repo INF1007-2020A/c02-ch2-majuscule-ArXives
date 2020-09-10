e440a3bc965f98f51371aa8598c857157c04f9d9
ArXives <imane.znadi@polymtl.ca>
2020-09-10 22:09:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    
+    distance entre min_et_maj
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
