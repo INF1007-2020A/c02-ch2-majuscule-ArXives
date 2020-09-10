3d40222dad6a536653965c0e89e9cbbd5b34dcc9
ArXives <imane.znadi@polymtl.ca>
2020-09-10 22:14:47 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        if ord('a') <= ord(lettre) and ord(lettre) <= ord
+        if ord('a') <= ord(lettre) and ord(lettre) <= 122
         lettre = ord(lettre) - distance_entre_min_et_maj 
         resultat += chr(lettre)
     return resultat
